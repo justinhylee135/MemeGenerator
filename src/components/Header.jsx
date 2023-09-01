0a1ee99f5f3dadfb10ed532552e8515b98070cee
Justin Lee <justinhylee135@gmail.com>
2023-08-31 22:08:07 -0400

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <img className="header--image" src={TrollFace} />
       <h2 className="header--title">Meme Generator</h2>
       <a
-        href="https://github.com/justinhylee135?tab=repositories"
+        href="https://github.com/justinhylee135/MemeGenerator"
         target="_blank"
         rel="noopener noreferrer"
         className="header--project"
